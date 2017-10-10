7d80e74899da5b566b6c362b90b02510c1a946b9
jameswquinn <jameswquinn@users.noreply.github.com>
2017-10-10 22:01:46 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var PurifyCSSPlugin = require('purifycss-webpack');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 var path = require("path");
 var glob = require('glob');
 var glob = require('glob-all');
@@ -93,33 +93,6 @@ module.exports = {
       ],
       mobile: true,
       lang: 'en',
-      links: [
-        'https://fonts.googleapis.com/css?family=Roboto',
-        {
-          href: '/apple-touch-icon.png',
-          rel: 'apple-touch-icon',
-          sizes: '180x180'
-        },
-        {
-          href: '/favicon-32x32.png',
-          rel: 'icon',
-          sizes: '32x32',
-          type: 'image/png'
-        },
-        {
-          href: '/favicon-16x16.png',
-          rel: 'icon',
-          sizes: '16x16',
-          type: 'image/png'
-        }
-      ],
-      inlineManifestWebpackName: 'webpackManifest',
-      title: 'My App',
-      window: {
-        env: {
-          apiHost: 'http://myapi.com/api/v1'
-        }
-      },
       title: 'Project Demo',
       // minify: {
       //     collapseWhitespace: true
@@ -138,14 +111,45 @@ module.exports = {
             template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
         }),
         */
+        new FaviconsWebpackPlugin({
+    // Your source logo
+    logo: './src/logo.png',
+    // The prefix for all image files (might be a folder or a name)
+    prefix: 'icons-[hash]/',
+    // Emit all stats of the generated icons
+    emitStats: false,
+    // The name of the json containing all favicon information
+    statsFilename: 'iconstats-[hash].json',
+    // Generate a cache file with control hashes and
+    // don't rebuild the favicons until those hashes change
+    persistentCache: true,
+    // Inject the html into the html-webpack-plugin
+    inject: true,
+    // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+    background: '#fff',
+    // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+    title: 'Webpack App',
+
+    // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: false,
+      coast: false,
+      favicons: true,
+      firefox: false,
+      opengraph: false,
+      twitter: false,
+      yandex: false,
+      windows: false
+    }
+  }),
         new ExtractTextPlugin({
             filename: 'app.css',
             disable: false,
             allChunks: true
         }),
-        new PurifyCSSPlugin({
-      // Give paths to parse for rules. These should be absolute!
-      paths: glob.sync(path.join(__dirname, 'src/*.html')),
-    })
+
+
     ]
 }
